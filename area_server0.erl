e1ee5093a19ea43f81b1dbef684ee603fcfdf286
zhaojianc <zhaojianc@yonyou.com>
2017-12-21 13:02:29 +0800

    add

--- area_server0.erl
+++ area_server0.erl
@@ -1,15 +1,22 @@
 -module(area_server0).
--export([loop/0]).
+-export([loop/0,rpc/2]).
+
+rpc(Pid,Request)->
+    Pid! {self(),Request},
+    receive
+	Response->
+	    Response
+    end.
 
 loop()->
     receive
-	{rectangle,Width,Ht}->
-	    io:format("Area of rectangle is ~p~n",[Width*Ht]),
+	{From,{rectangle,Width,Ht}}->
+	    From!Width*Ht,
 	    loop();
-	{circle,R} ->
-	    io:format("Area of circle is ~p~n",[3.14159*R*R]),
+	{From,{circle,R}} ->
+	    From!3.14159*R*R,
 	    loop();
-	Other->
-	    io:format("I don't know what the area of a  ~p is ~n",Other),
+	{From,Other}->
+	    From!{error,Other},
 	    loop()
     end.
