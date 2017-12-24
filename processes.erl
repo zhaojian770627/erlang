e5a1a47afd4630d86e06033de3d64f7024da3be1
zhaojian770627 <zhaojian770627@163.com>
2017-12-24 21:29:54 +0800

    add

--- processes.erl
+++ processes.erl
@@ -4,17 +4,17 @@
 max(N)->
     Max=erlang:system_info(process_limit),
     io:format("Maximum allowed processes:~p~n",[Max]),
-    staticstics(runtime),
-    staticstics(wall_clock),
+    statistics(runtime),
+    statistics(wall_clock),
     L=for(1,N,fun()->
 		      spawn(fun()->
 				    wait() end) end),
-    {_,Time1}=staticstics(runtime),
-    {_,Time2}=staticstics(wall_clock),
+    {_,Time1}=statistics(runtime),
+    {_,Time2}=statistics(wall_clock),
     lists:foreach(fun(Pid)->
 			  Pid!die end,L),
     U1=Time1*1000/N,
-    U2=TIme2*1000/N,
+    U2=Time2*1000/N,
     io:format("Process spawn time=~p(~p) microseconds~n",[U1,U2]).
 
 wait()->
@@ -27,3 +27,4 @@ for(N,N,F)->
     [F()];
 for(I,N,F) ->
     [F()|for(I+1,N,F)].
+
